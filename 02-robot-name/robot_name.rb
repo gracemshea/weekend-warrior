8bdfc8c8c2e9fe925e2fd9b073dda6f04a8d07fc
gracemshea <33272959+gracemshea@users.noreply.github.com>
2019-03-03 17:54:23 -0800

    Update robot_name.rb

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,15 @@
+class Robot
+  attr_accessor :name
 
+  def initialize
+    robot_name = String.new
+    letter_set = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+    2.times do
+      random = rand(0..25)
+      robot_name += letter_set[random]
+    end
+    3.times do
+      robot_name += rand(0..9).to_s
+    end
+    @name = robot_name
+  end
