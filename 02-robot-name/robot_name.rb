6542ce01585e77d48e0f3322ad05f189bdedf112
gracemshea <33272959+gracemshea@users.noreply.github.com>
2019-03-03 17:54:55 -0800

    Update robot_name.rb

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -13,3 +13,17 @@ class Robot
     end
     @name = robot_name
   end
+
+  def reset
+    robot_name = String.new
+    letter_set = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+    2.times do
+      random = rand(0..25)
+      robot_name += letter_set[random]
+    end
+    3.times do
+      robot_name += rand(0..9).to_s
+    end
+    @name = robot_name
+  end
+end
