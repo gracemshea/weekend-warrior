29f5d3bcaea2cdbe1070216b3167bc85ea02f86d
Grace Shea <grace.m.shea@gmail.com>
2019-03-10 16:03:17 -0700

    first draft of hamming method, unskipped tests in spec

---
+++ 03-hamming/hamming.rb
@@ -0,0 +1,16 @@
+
+class Hamming
+  def compute(strand1, strand2)
+    if strand1.length != strand2.length
+      raise ArgumentError, "DNA strands must be the same length! (Strand 1 has #{strand1.length} letters while Strand 2 has #{strand2.length}letters.)"
+    end
+
+    hamming_count = 0
+    (strand1.split(//)).zip(strand2.split(//)).each do |char|
+      if char[0] != char[1]
+        hamming_count += 1
+      end
+    end
+    return hamming_count
+  end
+end
